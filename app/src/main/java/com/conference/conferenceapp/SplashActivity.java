d5c1280db8ad7e202983abe3b37f20cdd12051ac
shahkishan020 <kshah1125@live.com>
2017-06-29 11:57:44 +0530

    initial commit

---
+++ app/src/main/java/com/conference/conferenceapp/SplashActivity.java
@@ -0,0 +1,21 @@
+package com.conference.conferenceapp;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                finish();
+            }
+        },3000);
+    }
+}
